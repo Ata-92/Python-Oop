c99a27929fa37691a9bdc1103b70d5db1a7ca124
Ata-92 <atakal92@gmail.com>
2021-07-05 14:19:42 +0300

    Add a static method

--- oop.py
+++ oop.py
@@ -28,30 +28,49 @@
 
 # SELF keyword
 
+# class Person:
+#     name = "Barry"
+#     age = 44
+
+#     def test(self):
+#         pass
+
+#     def get_details(self):
+#         print("Name:", self.name, "Age:", self.age, "Location:", self.location)
+
+#     def set_details(self):
+#         self.name = "Rafe"
+#         print(self.name)
+
+#         self.location = "Turkey"
+#         print(self.location)
+
+# person = Person()
+# person.test()
+# Person.test(person)
+
+# person.set_details()
+# person.get_details()
+# print(person.location)
+# print(Person.location)
+# print(Person.name)
+
+# Static Methods
+
 class Person:
-    name = "Barry"
+    name = 'Barry'
     age = 44
-
-    def test(self):
-        pass
-
-    def get_details(self):
-        print("Name:", self.name, "Age:", self.age, "Location:", self.location)
 
     def set_details(self):
         self.name = "Rafe"
         print(self.name)
-
         self.location = "Turkey"
-        print(self.location)
+
+    @staticmethod
+    def salute():
+        print("Hi there!", Person.name)
 
 person = Person()
-person.test()
-Person.test(person)
-
 person.set_details()
-person.get_details()
-print(person.location)
-print(Person.location)
-print(Person.name)
-
+person.salute()
+Person.salute()
