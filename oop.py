c6c72f2a8585d5e93b25af7a0c17bbe8b2fe0226
Ata-92 <atakal92@gmail.com>
2021-07-05 13:47:07 +0300

    Create two instances from Person

--- oop.py
+++ oop.py
@@ -1,10 +1,19 @@
-def printtype(data):
-    for i in data:
-        print(i, type(i))
+# def printtype(data):
+#     for i in data:
+#         print(i, type(i))
 
-testdata = [123, 'Barry', [1,2,3], (1,2,3), {1,2,3}, True, lambda x: x, {'name':'Barry'}]
-printtype(testdata)
+# testdata = [123, 'Barry', [1,2,3], (1,2,3), {1,2,3}, True, lambda x: x, {'name':'Barry'}]
+# printtype(testdata)
 
 class Person:
     name = 'Barry'
     age = 44
+
+person1 = Person()
+person2 = Person()
+
+print(person1.name)
+print(person2.name)
+
+
+
